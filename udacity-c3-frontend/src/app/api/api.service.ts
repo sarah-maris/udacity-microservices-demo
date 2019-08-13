e374dc443883903e029905f81bc357e4c563a4e7
Sarah Maris <sarah@sarahmaris.com>
2019-08-12 22:28:54 -0400

    Adjust frontend to work with split backend

--- udacity-c3-frontend/src/app/api/api.service.ts
+++ udacity-c3-frontend/src/app/api/api.service.ts
@@ -1,70 +1,82 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders,  HttpErrorResponse, HttpRequest, HttpEvent } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpRequest,
+  HttpEvent
+} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { FeedItem } from '../feed/models/feed-item.model';
 import { catchError, tap, map } from 'rxjs/operators';
 
 const API_HOST = environment.apiHost;
+const API_FEED_HOST = environment.apiFeedHost;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   token: string;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   handleError(error: Error) {
     alert(error.message);
   }
 
   setAuthToken(token) {
-    this.httpOptions.headers = this.httpOptions.headers.append('Authorization', `jwt ${token}`);
+    this.httpOptions.headers = this.httpOptions.headers.append(
+      'Authorization',
+      `jwt ${token}`
+    );
     this.token = token;
   }
 
-  get(endpoint): Promise<any> {
-    const url = `${API_HOST}${endpoint}`;
-    const req = this.http.get(url, this.httpOptions).pipe(map(this.extractData));
+  get(endpoint, feed?: boolean): Promise<any> {
+    const url = feed ? `${API_FEED_HOST}${endpoint}` : `${API_HOST}${endpoint}`;
+    const req = this.http
+      .get(url, this.httpOptions)
+      .pipe(map(this.extractData));
 
-    return req
-            .toPromise()
-            .catch((e) => {
-              this.handleError(e);
-              throw e;
-            });
+    return req.toPromise().catch(e => {
+      this.handleError(e);
+      throw e;
+    });
   }
 
-  post(endpoint, data): Promise<any> {
-    const url = `${API_HOST}${endpoint}`;
-    return this.http.post<HttpEvent<any>>(url, data, this.httpOptions)
-            .toPromise()
-            .catch((e) => {
-              this.handleError(e);
-              throw e;
-            });
+  post(endpoint, data, feed?: boolean): Promise<any> {
+    const url = feed ? `${API_FEED_HOST}${endpoint}` : `${API_HOST}${endpoint}`;
+    return this.http
+      .post<HttpEvent<any>>(url, data, this.httpOptions)
+      .toPromise()
+      .catch(e => {
+        this.handleError(e);
+        throw e;
+      });
   }
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
-    const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
+    const signed_url = (await this.get(
+      `${endpoint}/signed-url/${file.name}`,
+      true
+    )).url;
+    console.log(signed_url);
+    const headers = new HttpHeaders({ 'Content-Type': file.type });
+    const req = new HttpRequest('PUT', signed_url, file, {
+      headers: headers,
+      reportProgress: true // track progress
+    });
 
-    const headers = new HttpHeaders({'Content-Type': file.type});
-    const req = new HttpRequest( 'PUT', signed_url, file,
-                                  {
-                                    headers: headers,
-                                    reportProgress: true, // track progress
-                                  });
-
-    return new Promise ( resolve => {
-        this.http.request(req).subscribe((resp) => {
-        if (resp && (<any> resp).status && (<any> resp).status === 200) {
-          resolve(this.post(endpoint, payload));
+    return new Promise(resolve => {
+      this.http.request(req).subscribe(resp => {
+        if (resp && (<any>resp).status && (<any>resp).status === 200) {
+          resolve(this.post(endpoint, payload, true));
         }
       });
     });
@@ -73,6 +85,6 @@ export class ApiService {
   /// Utilities
   private extractData(res: HttpEvent<any>) {
     const body = res;
-    return body || { };
+    return body || {};
   }
 }
